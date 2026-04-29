4accbda90eb8fe2108c601d057b8c54ca36adcb2
Antonin <antochevolleau@gmail.com>
2026-04-29 15:26:35 +0200

    fix: update gitignore and use VITE_API_URL env var

--- frontend/app/src/constants/api-endpoints.js
+++ frontend/app/src/constants/api-endpoints.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3001/api/v1";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3001/api/v1";
 
 /**
  * API endpoints configuration object containing all API routes used in the application.
